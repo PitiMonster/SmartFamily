e619f190d55bbc16e726557ffc943954beaa60e2
Pitimonster <piotrszym159@gmail.com>
2021-10-30 13:26:24 +0200

    feat: desktop calendar events part done

---
+++ web-app/src/components/ListItem/index.tsx
@@ -0,0 +1,38 @@
+import classes from "./index.module.scss";
+
+import IconButton from "@mui/material/IconButton";
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
+
+const ListItem: React.FC<{
+  photo?: string;
+  primaryText: string;
+  secondaryText?: string;
+  trailingText: string;
+  onClick: () => void;
+}> = ({ photo, primaryText, secondaryText, trailingText, onClick }) => {
+  return (
+    <li className={classes.container}>
+      <div className={classes.beginningPart}>
+        {photo && <img className={classes.photo} src={photo} alt="list item" />}
+        <div className={classes.beginningPart__texts}>
+          <p className={classes.primary}>{primaryText}</p>
+          {secondaryText && (
+            <p className={classes.secondary}>{secondaryText}</p>
+          )}
+        </div>
+      </div>
+      <div className={classes.trailingPart}>
+        <p className={classes.trailingPart__text}>{trailingText}</p>
+        <IconButton
+          aria-label="toggle password visibility"
+          onClick={onClick}
+          onMouseDown={onClick}
+        >
+          <KeyboardArrowRightIcon />
+        </IconButton>
+      </div>
+    </li>
+  );
+};
+
+export default ListItem;
--- web-app/src/pages/Calendar/CalendarPage/index.tsx
+++ web-app/src/pages/Calendar/CalendarPage/index.tsx
@@ -1,10 +1,12 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Calendar from "react-calendar";
+import moment from "moment";
 
 import classes from "./index.module.scss";
 import "./calendar-styles.scss";
 
 import ContentLayout from "../../../layout/ContentLayout";
+import ListItem from "../../../components/ListItem";
 
 const calendarButton = () => {
   return <button className={classes.myCalendarButton} />;
@@ -12,20 +14,101 @@ const calendarButton = () => {
 
 const CalendarPage: React.FC = () => {
   const [value, onChange] = useState(new Date());
+  const [selectedDay, setSelectedDay] = useState<String>(moment().format("DD"));
+  const [selectedMonth, setSelectedMonth] = useState<String>(
+    moment().format("MMMM")
+  );
+  const [events, setEvents] = useState<
+    | React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLDivElement>,
+        HTMLDivElement
+      >[]
+    | undefined
+  >([]);
+
+  useEffect(() => {
+    const eventsData = [
+      {
+        primaryText: "event 1",
+        trailingText: "trailing 1",
+        onClick: () => {},
+      },
+      {
+        primaryText: "event 1",
+        trailingText: "trailing 1",
+        onClick: () => {},
+      },
+      {
+        primaryText: "event 1",
+        trailingText: "trailing 1",
+        onClick: () => {},
+      },
+      {
+        primaryText: "event 1",
+        trailingText: "trailing 1",
+        onClick: () => {},
+      },
+      {
+        primaryText: "event 1",
+        trailingText: "trailing 1",
+        onClick: () => {},
+      },
+      {
+        primaryText: "event 1",
+        trailingText: "trailing 1",
+        onClick: () => {},
+      },
+      {
+        primaryText: "event 1",
+        trailingText: "trailing 1",
+        onClick: () => {},
+      },
+      {
+        primaryText: "event 1",
+        trailingText: "trailing 1",
+        onClick: () => {},
+      },
+      {
+        primaryText: "event 1",
+        trailingText: "trailing 1",
+        onClick: () => {},
+      },
+      {
+        primaryText: "event 1",
+        trailingText: "trailing 1",
+        onClick: () => {},
+      },
+    ];
+
+    const newEvents = eventsData.map((event) => <ListItem {...event} />);
+    setEvents(newEvents);
+  }, []);
+
+  const onChangeCalendar = (date: Date) => {
+    const mDate = moment(date);
+    onChange(date);
+    setSelectedDay(mDate.format("DD"));
+    setSelectedMonth(mDate.format("MMMM"));
+  };
 
   return (
     <ContentLayout>
       <div className={classes.container}>
         <div className={classes.info}>
-          <p className={classes.selectedDay}>28</p>
+          <div className={classes.date}>
+            <p className={classes.selectedDay}>{selectedDay}</p>
+            <p className={classes.selectedMonth}>{selectedMonth}</p>
+          </div>
           <div className={classes.events}>
             <p className={classes.events__title}>Events</p>
-            <ul className={classes.events__list}></ul>
+            <div className={classes.listContainer}>
+              <ul className={classes.events__list}>{events}</ul>
+            </div>
           </div>
         </div>
         <Calendar
           className={classes.calendarContainer}
-          onChange={onChange}
+          onChange={onChangeCalendar}
           value={value}
           locale="en-GB"
         />
