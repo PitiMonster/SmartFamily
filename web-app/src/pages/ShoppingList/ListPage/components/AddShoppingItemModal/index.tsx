16a2d622cf015637b6f42e3e7e74635871192dbe
Pitimonster <piotrszym159@gmail.com>
2021-11-06 19:27:48 +0100

    feat: add modify expense modal + shopping list responsiveness

---
+++ web-app/src/pages/ShoppingList/ListPage/components/AddShoppingItemModal/index.tsx
@@ -0,0 +1,64 @@
+import { useEffect, useState } from "react";
+
+import TextareaAutosize from "@mui/material/TextareaAutosize";
+import FormControl from "@mui/material/FormControl";
+
+import classes from "./index.module.scss";
+
+import { useAppDispatch } from "../../../../../hooks";
+import { updateBackdrop } from "../../../../../store/utils/action";
+
+import TextInput from "../../../../../components/inputs/TextInput";
+import MainButton from "../../../../../components/buttons/MainButton";
+
+const AddExpenseModal: React.FC<{
+  title: string;
+  name?: string;
+  price?: number;
+  description?: string;
+}> = ({ title, name, price, description }) => {
+  const dispatch = useAppDispatch();
+  const [itemName, setItemName] = useState<string>("");
+  const [expensePrice, setExpensePrice] = useState<string>("");
+  const [expenseDescription, setExpenseDescription] = useState<string>("");
+
+  useEffect(() => {
+    dispatch(updateBackdrop(true));
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (name && price && description) {
+      setItemName(name);
+      setExpensePrice(price.toString());
+      setExpenseDescription(description);
+    }
+  }, [name, price, description]);
+
+  return (
+    <div className={classes.modal}>
+      <p className={classes.title}>{title}</p>
+      <FormControl variant="standard" color="primary" className={classes.input}>
+        <TextInput text={itemName} setText={setItemName} label="Item name" />
+      </FormControl>
+      <FormControl variant="standard" color="primary" className={classes.input}>
+        <TextInput
+          text={expensePrice}
+          setText={setExpensePrice}
+          label="Price"
+        />
+      </FormControl>
+
+      <TextareaAutosize
+        aria-label="minimum height"
+        minRows={10}
+        maxRows={10}
+        placeholder="Item description"
+        value={expenseDescription}
+        onChange={(event) => setExpenseDescription(event.target.value)}
+      />
+      <MainButton isOutline={true} text="Save" onClick={() => {}} />
+    </div>
+  );
+};
+
+export default AddExpenseModal;
--- web-app/src/pages/ShoppingList/ListPage/index.tsx
+++ web-app/src/pages/ShoppingList/ListPage/index.tsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 
 import AddIcon from "@mui/icons-material/Add";
-import Button from "@mui/material/Button";
 import Collapse from "@mui/material/Collapse";
 import IconButton from "@mui/material/IconButton";
 import List from "@mui/material/List";
@@ -12,21 +11,52 @@ import { TransitionGroup } from "react-transition-group";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import Checkbox from "@mui/material/Checkbox";
+import { Stack } from "@mui/material";
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 
 import classes from "./index.module.scss";
 
+import { useAppSelector } from "../../../hooks";
+
 import shopImgPath from "../../../assets/images/shoppingListShop.jpg";
-import { HtmlElements } from "../../../types";
 
 import ContentLayout from "../../../layout/ContentLayout";
+import AddShoppingItemModal from "./components/AddShoppingItemModal";
+
+interface RenderItemOptions {
+  name: string;
+  username: string;
+  price: number;
+  checked: boolean;
+  description: string;
+  handleRemoveItem: (name: string) => void;
+  onClick: () => void;
+}
+
+interface SelectedShoppingItemData {
+  title: string;
+  name: string;
+  price: number;
+  description: string;
+}
 
 const ListPage: React.FC = () => {
+  const [selectedShoppingItemData, setSelectedShoppingItemData] =
+    useState<SelectedShoppingItemData>({
+      title: "Add item",
+      name: "",
+      price: 0,
+      description: "",
+    });
+  const [isAddShoppingItemModal, setIsAddShoppingItemModal] =
+    useState<boolean>(false);
   const [shoppingList, setShoppingList] = useState<
     {
       name: string;
       username: string;
       price: number;
       checked: boolean;
+      description: string;
     }[]
   >([]);
   const [checkedShoppingList, setCheckedShoppingList] = useState<
@@ -35,8 +65,23 @@ const ListPage: React.FC = () => {
       username: string;
       price: number;
       checked: boolean;
+      description: string;
     }[]
   >([]);
+
+  const isBackdrop = useAppSelector((state) => state.utils.isBackdrop);
+
+  useEffect(() => {
+    if (!isBackdrop) {
+      setIsAddShoppingItemModal(false);
+      setSelectedShoppingItemData({
+        title: "Add item",
+        name: "",
+        price: 0,
+        description: "",
+      });
+    }
+  }, [isBackdrop]);
 
   useEffect(() => {
     const items = [
@@ -45,80 +90,138 @@ const ListPage: React.FC = () => {
         username: "Pitimonster",
         price: 12.3,
         checked: false,
+        description: "Temp description",
       },
       {
         name: "Name2",
         username: "Pitimonster",
         price: 12.3,
         checked: false,
+        description: "Temp description",
       },
       {
         name: "Name3",
         username: "Pitimonster",
         price: 12.3,
         checked: false,
+        description: "Temp description",
       },
       {
         name: "Name4",
         username: "Pitimonster",
         price: 12.3,
         checked: false,
+        description: "Temp description",
       },
       {
         name: "Name5",
         username: "Pitimonster",
         price: 12.3,
         checked: false,
+        description: "Temp description",
       },
       {
         name: "Name6",
         username: "Pitimonster",
         price: 12.3,
         checked: false,
+        description: "Temp description",
       },
       {
         name: "Name7",
         username: "Pitimonster",
         price: 12.3,
         checked: false,
+        description: "Temp description",
       },
       {
         name: "Name8",
         username: "Pitimonster",
         price: 12.3,
         checked: false,
+        description: "Temp description",
+      },
+      {
+        name: "Name8",
+        username: "Pitimonster",
+        price: 12.3,
+        checked: false,
+        description: "Temp description",
+      },
+      {
+        name: "Name8",
+        username: "Pitimonster",
+        price: 12.3,
+        checked: false,
+        description: "Temp description",
+      },
+      {
+        name: "Name8",
+        username: "Pitimonster",
+        price: 12.3,
+        checked: false,
+        description: "Temp description",
+      },
+      {
+        name: "Name8",
+        username: "Pitimonster",
+        price: 12.3,
+        checked: false,
+        description: "Temp description",
+      },
+      {
+        name: "Name10",
+        username: "Pitimonster",
+        price: 12.3,
+        checked: false,
+        description: "Temp description",
       },
     ];
     setShoppingList(items);
   }, []);
-
-  interface RenderItemOptions {
-    name: string;
-    username: string;
-    price: number;
-    checked: boolean;
-    handleRemoveItem: (name: string) => void;
-    onClick: () => void;
-  }
 
   const renderItem = ({
     name,
     username,
     price,
     checked,
+    description,
     handleRemoveItem,
     onClick,
   }: RenderItemOptions) => (
     <ListItem
+      sx={{ paddingLeft: 0 }}
       secondaryAction={
-        <IconButton
-          edge="end"
-          aria-label="delete"
-          title="Delete"
-          onClick={() => handleRemoveItem(name)}
-        >
-          <DeleteIcon />
-        </IconButton>
+        <Stack direction="row" alignItems="center">
+          <ListItemText
+            primary={`${price.toFixed(2)}zł`}
+            sx={{ paddingRight: "1rem" }}
+          />
+          <IconButton
+            aria-label="delete"
+            title="Delete"
+            onClick={() => handleRemoveItem(name)}
+            color="error"
+          >
+            <DeleteIcon />
+          </IconButton>
+          <IconButton
+            aria-label="info"
+            title="Info"
+            onClick={() => {
+              setSelectedShoppingItemData({
+                title: "Modify item",
+                name,
+                price,
+                description,
+              });
+              setIsAddShoppingItemModal(true);
+            }}
+            sx={{ paddingLeft: "1rem", paddingRight: "1rem" }}
+          >
+            <KeyboardArrowRightIcon />
+          </IconButton>
+        </Stack>
       }
     >
       <ListItemButton role={undefined} onClick={onClick} dense>
@@ -132,7 +235,7 @@ const ListPage: React.FC = () => {
             color={checked ? "info" : "primary"}
           />
         </ListItemIcon>
-        <ListItemText id={name} primary={name} />
+        <ListItemText id={name} primary={name} secondary={username} />
       </ListItemButton>
     </ListItem>
   );
@@ -149,11 +252,16 @@ const ListPage: React.FC = () => {
         <div className={classes.container__list}>
           <div className={classes.header}>
             <p className={classes.header__title}>Shopping List</p>
-            <IconButton color="primary" onClick={() => {}}>
+            <IconButton
+              color="primary"
+              onClick={() => {
+                setIsAddShoppingItemModal(true);
+              }}
+            >
               <AddIcon fontSize="large" />
             </IconButton>
           </div>
-          <div className={classes.list}>
+          <div className={classes.lists}>
             <List>
               <TransitionGroup>
                 {shoppingList.map((item) => (
@@ -163,6 +271,7 @@ const ListPage: React.FC = () => {
                       username: item.username,
                       price: item.price,
                       checked: item.checked,
+                      description: item.description,
                       handleRemoveItem: (name) => {},
                       onClick: () => {
                         setShoppingList((prev) => {
@@ -181,8 +290,6 @@ const ListPage: React.FC = () => {
                 ))}
               </TransitionGroup>
             </List>
-          </div>
-          <div className={classes.list}>
             <List>
               <TransitionGroup>
                 {checkedShoppingList.map((item) => (
@@ -192,6 +299,7 @@ const ListPage: React.FC = () => {
                       username: item.username,
                       price: item.price,
                       checked: item.checked,
+                      description: item.description,
                       handleRemoveItem: (name) => {},
                       onClick: () => {
                         setCheckedShoppingList((prev) => {
@@ -210,6 +318,9 @@ const ListPage: React.FC = () => {
           </div>
         </div>
       </div>
+      {isAddShoppingItemModal && (
+        <AddShoppingItemModal {...selectedShoppingItemData} />
+      )}
     </ContentLayout>
   );
 };
